663702d7cc0e76c9d20683720759b94be6428b57
irrawaddy28 <xad890@gmail.com>
2025-06-27 21:27:46 -0700

    completed all changes

---
+++ Exercise_1.py
@@ -0,0 +1,151 @@
+'''
+322 Coin Change
+https://leetcode.com/problems/coin-change/description/
+
+You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+
+Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+You may assume that you have an infinite number of each kind of coin.
+
+Assume N denominations of coins and target sum is K.
+
+Example 1:
+Input: coins = [1,2,5], amount = 11
+Output: 3
+Explanation: 11 = 5 + 5 + 1
+
+Example 2:
+Input: coins = [2], amount = 3
+Output: -1
+
+Example 3:
+Input: coins = [1], amount = 0
+Output: 0
+
+Solution:
+1. Brute Force (Recursion): Starting from the target sum, for each coin coins[i], we can either include it or exclude it. If we include it, we subtract its value from sum and recursively try to make the remaining amount with the same coin denominations. If we exclude it, we move to the next coin in the list.
+Thus, at each step, we have N choices of coin denominations. At the max, we take K such steps (since least denomination is 1).
+Time: O(N^K), Space: O(N)
+
+2. Recursion with memoization (top-down approach): Keep track of overlapping sub-problem using a hash map. The key of the hash map is the remaining sum to be computed and the value is the minimum no. of coins to reach the remaining sum. If we encounter it again, we just use the hash map instead of repeating the computation.
+Time: O(N*K), Space: O(N*K)
+
+3. Tabulation (bottom-up approach): Construct a 2-D DP array of size
+(N+1) x (K)+1. Initialize it and fill the array using:
+case_0 = dp[i-1][j]
+case_1 = 1 + dp[i][j - coins[i-1]]
+dp[i][j] = min(case_0, case_1)
+Time: O(N*K), Space: O(N*K)
+'''
+import time
+import numpy as np
+
+# def coin_change(coins, amount):
+#     if amount < 0:
+#         return float('inf')
+#     if amount == 0:
+#         return -1
+
+#     m = float('inf')
+#     for c in coins:
+#         nc = 1 + coin_change(coins, amount - c)
+#         m = min(m, nc)
+#     return m
+
+# def coin_change_DP(coins, amount, D):
+#     if amount < 0:
+#         return float('inf')
+#     if amount == 0:
+#         return 0
+
+#     m = float('inf')
+#     for c in coins:
+#         if amount -c not in D:
+#             D[amount -c] = coin_change_DP(coins, amount - c, D)
+#         nc = 1 + D[amount -c]
+#         m = min(m, nc)
+#     return m
+
+
+def coin_change(coins, amount):
+    '''
+    recursion
+    '''
+    def recurse(coins, amount, index, num_coins):
+        N = len(coins)
+        if amount == 0:
+            return num_coins
+        if index == N or amount < 0:
+            return -1
+
+        # 0-case (don't take)
+        case_0 = recurse(coins, amount, index+1, num_coins)
+
+        # 1-case (take)
+        case_1 = recurse(coins, amount - coins[index], index, num_coins+1)
+
+        if case_0 == -1:
+            return case_1
+
+        if case_1 == -1:
+            return case_0
+
+        return min(case_0, case_1)
+
+    N = len(coins)
+    if N == 0 or amount <= 0:
+        return 0
+
+    return recurse(coins, amount, 0, 0)
+
+def coin_change_DP(coins, amount):
+    N = len(coins)
+    if N == 0 or amount <= 0:
+        return 0
+
+    dp = np.zeros((N+1, amount+1))
+    for j in range(1, amount+1):
+        dp[0][j] = amount + 1
+
+    for i in range(1, N+1):
+        for j in range(1, amount + 1):
+            if coins[i-1] > j:
+                dp[i][j] = dp[i-1][j]
+            else:
+                case_0 = dp[i-1][j]
+                case_1 = 1 + dp[i][j - coins[i-1]]
+                dp[i][j] = min(case_0, case_1)
+
+    if dp[N][amount] == amount + 1:
+        return -1
+
+    return dp[N][amount]
+
+
+def run_coin_change():
+    tests = [([1,2,5], 11, 3),  ([2], 3, -1), ([9, 6, 5, 1], 19, 3), ([9, 6, 5, 1], 30, 4)]
+
+    for test in tests:
+        coins, amount, ans = test[0], test[1], test[2]
+        start = time.time()*1000
+        num_coins = coin_change(coins, amount)
+        elapsed =  time.time()*1000 - start
+        # if num_coins == float('inf'):
+        #     num_coins = -1
+        print(f"\ncoins = {coins}")
+        print(f"Min number of coins to reach target {amount} = {num_coins}, time = {elapsed:.2f} ms (recursion)")
+        print(f"Pass: {ans == num_coins}")
+
+    for test in tests:
+        coins, amount, ans = test[0], test[1], test[2]
+        start = time.time()*1000
+        num_coins = coin_change_DP(coins, amount)
+        elapsed =  time.time()*1000 - start
+        # if num_coins == float('inf'):
+        #     num_coins = -1
+        print(f"\ncoins = {coins}")
+        print(f"Min number of coins to reach target {amount} = {num_coins}, time = {elapsed:.2f} ms (DP)")
+        print(f"Pass: {ans == num_coins}")
+
+
+run_coin_change()
---
+++ Exercise_2.py
@@ -0,0 +1,141 @@
+'''
+198 House Robber
+https://leetcode.com/problems/house-robber/description/
+
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+Example 1:
+Input: nums = [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+Total amount you can rob = 1 + 3 = 4.
+
+Example 2:
+Input: nums = [2,7,9,3,1]
+Output: 12
+Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+Total amount you can rob = 2 + 9 + 1 = 12.
+
+Example 3:
+Input: nums = [1,2]
+Output: 2
+Explanation: Rob house 2 (money = 2).
+Total amount you can rob = 2.
+
+Example 4: (interesting case)
+Input: nums = [2, 7, 3, 1, 4, 2, 1, 8]
+Output: 19
+Explanation: 7 + 4 + 8 = 19
+
+Solution:
+1. Brute Force (Recursion): At each house, we have two choices: steal or skip. We do this recursively for each house. Thus for N houses, we have 2^N possibilities.
+Time: O(2^N), Space: O(N)
+
+2. Recursion with memoization (top-down approach): We keep a track of the max possible loot at each house using a hash map to avoid computing repeated sub-problems. Since the max loot at every house is computed only once, time complexity is O(N).
+Time: O(N), Space: O(N)
+
+3. Tabulation (bottom-up approach):: Construct 1-d DP array of size N+1, where dp[i] = max loot until ith house (i.e. 0,...,i)
+Initialize: dp[0] = 0        (since no loot before the 1st house)
+            dp[1] = house[0] (max loot until the 1st house)
+DP computation:
+            dp[2] = max(loot house 2 + prev loot, don't loose house 2 + prev loot)
+
+            if we loot house 2, then we know for sure we must have skipped house 1. That means our prev loot is from house 0 (dp[0])
+
+            if we don't loot house 2, then we must have looted house 1. That means our prev loot is from house 1 (dp[1])
+
+            Thus, for any i, dp[i] = max(house[i-1] + dp[i-2], dp[i-1])
+Time: O(N), Space: O(N)
+'''
+import time
+import numpy as np
+
+# def rob_DP(houses, D, i=-2):
+#     N = len(houses)
+#     # Handle edge case for i = N-2, N-1
+#     if N == 1:
+#         return houses[0]
+#     elif N == 2:
+#         return max(houses[0], houses[1])
+
+#     # Code below gets executed for i=-2,-1,0,..,N-3
+#     if i < 0:
+#         amount = 0
+#     else:
+#         amount = houses[i]
+#     m = 0
+#     for j in range(i+2,N):
+#         if j not in D:
+#             D[j] = rob_DP(houses, D, j)
+#         m = max(m, D[j])
+#     amount += m
+#     D[i] = amount
+#     return D[i]
+
+def rob(houses):
+    '''
+    recursion
+    '''
+    def recurse(houses, index, loot):
+        N = len(houses)
+
+        if index > N-1:
+            return loot
+
+        # 0-case (don't loot)
+        case_0 = recurse(houses, index+1, loot)
+
+        # 1-case (loot)
+        case_1 = recurse(houses, index+2, loot+houses[index])
+
+        return max(case_0, case_1)
+
+    N = len(houses)
+    # Handle edge cases
+    if N == 0:
+        return 0
+    if N == 1:
+        return houses([0])
+    if N == 2:
+        return max(houses[0], houses[1])
+
+    return recurse(houses, 0, 0)
+
+def rob_DP(houses):
+    '''
+    dymaic programming
+    '''
+    N = len(houses)
+    dp = np.zeros(N+1)
+
+    dp[0] = 0
+    dp[1] = houses[0]
+    for i in range(2, N+1): # O(N)
+        case_0 = dp[i-1]
+        case_1 = houses[i-1] + dp[i-2]
+        dp[i] = max(case_0,  case_1)
+    return dp[N]
+
+def run_rob():
+    tests = ([2,7,9,3,1], 12), ([1,2,3,1], 4), ([1,2], 2), ([2, 7, 3, 1, 4, 2, 1, 8], 19)
+    for test in tests:
+        houses, ans = test[0], test[1]
+        start = time.time()*1000
+        ans_rec = rob(houses)
+        elapsed =  time.time()*1000 - start
+        print(f"\nhouses = {houses}")
+        print(f"Max amt robbed={ans_rec}, time = {elapsed:.2f} ms (recursion)")
+        print(f"Pass: {ans == ans_rec}")
+
+    for test in tests:
+        houses, ans = test[0], test[1]
+        start = time.time()*1000
+        ans_dp = rob_DP(houses)
+        elapsed =  time.time()*1000 - start
+        print(f"\nhouses = {houses}")
+        print(f"Max amt robbed={ans_dp}, time = {elapsed:.2f} ms (DP)")
+        print(f"Pass: {ans == ans_dp}")
+
+run_rob()
